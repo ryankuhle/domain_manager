5e99aa0934ceceef88b711544eea91bc19021065
Ryan Kuhle <ryan@ryankuhle.com>
2017-04-21 10:56:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# CREATE DB
+
+# MENUS
+def view_menu():
+    print("Welcome")
+
+def view_all_entries():
+    pass
+
+def view_entry():
+    pass
+
+def create_entry():
+    pass
+
+def edit_entry():
+    pass
+
+def delete_entry():
+    pass
+
+if __name__ == "__main__":
+    view_menu()
